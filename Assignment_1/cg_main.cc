4c3b06bfd3c792b8647a82fd112201b68bc1acf8
Turk <turk.roy@protonmail.com>
2025-04-04 08:40:47 +0200

    new parallelisation

--- Assignment_1/cg_main.cc
+++ Assignment_1/cg_main.cc
@@ -1,6 +1,7 @@
 #include "cg.hh"
 #include <chrono>
 #include <iostream>
+#include <mpi.h>
 
 using clk = std::chrono::high_resolution_clock;
 using second = std::chrono::duration<double>;
@@ -13,9 +14,17 @@ market) Any matrix in that format can be used to test the code
 int
 main(int argc, char **argv)
 {
+  MPI_Init(&argc, &argv);
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
   if (argc < 2)
   {
+    if (rank == 0)
     std::cerr << "Usage: " << argv[0] << " [martix-market-filename]" << std::endl;
+
+    MPI_Finalize();
     return 1;
   }
 
@@ -30,11 +39,16 @@ main(int argc, char **argv)
   std::vector<double> x_s(n);
   std::fill(x_s.begin(), x_s.end(), 0.);
 
-  std::cout << "Call CG sparse on matrix size " << m << " x " << n << ")" << std::endl;
+  if (rank == 0)
+    std::cout << "Call CG sparse on matrix size " << m << " x " << n << ")" << std::endl;
+
   auto t1 = clk::now();
   sparse_solver.solve(x_s);
   second elapsed = clk::now() - t1;
-  std::cout << "Time for CG (sparse solver)  = " << elapsed.count() << " [s]\n";
+  
+  if (rank == 0)
+    std::cout << "Time for CG (sparse solver)  = " << elapsed.count() << " [s]\n";
 
+  MPI_Finalize();
   return 0;
 }
--- Assignment_1/matrix_coo.hh
+++ Assignment_1/matrix_coo.hh
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <mpi.h>
 
 #ifndef __MATRIX_COO_H_
 #define __MATRIX_COO_H_
@@ -18,18 +19,31 @@ public:
   void read(const std::string & filename);
 
   void mat_vec(const std::vector<double> & x, std::vector<double> & y) {
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    std::vector<double> y_local(y.size(), 0.0);
     std::fill_n(y.begin(), y.size(), 0.);
 
-    for (size_t z = 0; z < irn.size(); ++z) {
-      auto i = irn[z];
-      auto j = jcn[z];
-      auto a_ = a[z];
+    size_t nz_total = irn.size();
+    size_t chunck_size = (nz_total + size - 1) / size;
+    size_t z_start = rank * chunck_size;
+    size_t z_end = std::min(z_start + chunck_size, nz_total);
 
-      y[i] += a_ * x[j];
+    for (size_t z = z_start; z < z_end; ++z) {
+      int i = irn[z];
+      int j = jcn[z];
+      double a_ = a[z];
+
+      y_local[i] += a_ * x[j];
       if (m_is_sym and (i != j)) {
-        y[j] += a_ * x[i];
+        y_local[j] += a_ * x[i];
       }
     }
+
+    MPI_Allreduce(y_local.data(), y.data(), y.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
   }
 
   std::vector<int> irn;
